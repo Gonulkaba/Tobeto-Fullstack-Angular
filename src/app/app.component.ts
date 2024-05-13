1324850ef6006f001a34981a386d05afc14027e2
Gönül Kaba <gonul.kaba123@gmail.com>
2024-05-14 00:32:42 +0300

    add if and for examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,48 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [CommonModule, NavbarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title: string = 'Northwind Market';
   cartCount: number=0;
+
+  products: {name: string; price:number; discontinued: boolean}[]=[
+    //ürün listesi
+    {
+      name: "Çay",
+      price: 100,
+      discontinued: false
+    },
+    {
+      name: "Kahve",
+      price: 200,
+      discontinued: true
+    },
+    {
+      name: "Su",
+      price: 50,
+      discontinued: false
+    },
+    {
+      name: "Meyve suyu",
+      price: 20,
+      discontinued: true
+    },
+    {
+      name: "Maden suyu",
+      price: 25,
+      discontinued: false
+    }
+
+  ]
 
   onAddProductToCart(): void{
     this.cartCount +=1;
