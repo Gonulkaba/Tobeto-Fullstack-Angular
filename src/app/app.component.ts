9405d2c79524f3178802ba419fc4525061adf47e
Gönül Kaba <gonul.kaba123@gmail.com>
2024-05-15 23:55:24 +0300

    add routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NavbarComponent,FooterComponent],
+  imports: [CommonModule,RouterModule, NavbarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './routes/home-page/home-page.component';
+import { LoginPageComponent } from './routes/auth/login-page/login-page.component';
+import { authRoutes } from './routes/auth/auth.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '', // Route belirtilen path ile eşleştiğinde
+      component: HomePageComponent, // İlgili componenti AppComponent'ten başlayarak
+      // ilk karşılaştığı <router-outlet></router-outlet> etiketine yerleştirir.
+    },
+    ...authRoutes, // authRoutes içindeki tüm route'ları routes'a ekler.
+  // ... spread operator ile bir array içindeki tüm elemanlarını tek tek ilgili yere yerleştirir..
+  ];
+ 
