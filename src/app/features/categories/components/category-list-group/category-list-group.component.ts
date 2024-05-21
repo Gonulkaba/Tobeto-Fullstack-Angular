963e7ed90c7bf8bdbce429dc503da838adac6854
Gönül Kaba <gonul.kaba123@gmail.com>
2024-05-21 14:54:40 +0300

    add Input examples

--- src/app/features/categories/components/category-list-group/category-list-group.component.ts
+++ src/app/features/categories/components/category-list-group/category-list-group.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CategoryListItem } from '../../models/category-list-item';
+import { ListGroupComponent, ListGroupItem, ListGroupItems } from '../../../../shared/components/list-group/list-group.component';
 
 @Component({
   selector: 'app-category-list-group',
   standalone: true,
   imports: [
     CommonModule,
+    ListGroupComponent
   ],
   templateUrl: './category-list-group.component.html',
   styleUrl: './category-list-group.component.scss',
@@ -22,4 +24,15 @@ export class CategoryListGroupComponent {
     { id: 6, name: 'Produce'},
     { id: 7, name: 'Snacks'},
   ]; // Mock data
+
+  get categoryListGroupItems(): ListGroupItems {
+    return this.categoryList.map((category) => {
+      const categoryListItem: ListGroupItem = {
+        label: category.name,
+      };
+      return categoryListItem;
+    });
+
+    // return this.categoryList.map((category) => ({ label: category.name }));
+  }
 }
