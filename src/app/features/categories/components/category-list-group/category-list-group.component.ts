aae3599539689f695ce49832fd491b2545e4375c
Gönül Kaba <gonul.kaba123@gmail.com>
2024-05-21 17:54:49 +0300

    add Input and Output example

--- src/app/features/categories/components/category-list-group/category-list-group.component.ts
+++ src/app/features/categories/components/category-list-group/category-list-group.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 import { CategoryListItem } from '../../models/category-list-item';
 import { ListGroupComponent, ListGroupItem, ListGroupItems } from '../../../../shared/components/list-group/list-group.component';
 
@@ -15,6 +15,8 @@ import { ListGroupComponent, ListGroupItem, ListGroupItems } from '../../../../s
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoryListGroupComponent {
+  @ Output() changeSelect = new EventEmitter<number | null>();
+
   categoryList : CategoryListItem[] = [
     { id: 1, name: 'Beverages'},
     { id: 2, name: 'Canned Goods'},
@@ -25,9 +27,13 @@ export class CategoryListGroupComponent {
     { id: 7, name: 'Snacks'},
   ]; // Mock data
 
+  onChangeSelect(selectedItemId: string | null) {
+    this.changeSelect.emit(Number(selectedItemId));
+  }
   get categoryListGroupItems(): ListGroupItems {
     return this.categoryList.map((category) => {
       const categoryListItem: ListGroupItem = {
+        id: category.id.toString(),
         label: category.name,
       };
       return categoryListItem;
