9e71dfa1d8c1e397c5ffbbdcfc6f377b22c2b7d6
Gönül Kaba <gonul.kaba123@gmail.com>
2024-05-23 23:43:21 +0300

    add products service

--- src/app/features/products/models/product-list-item.ts
+++ src/app/features/products/models/product-list-item.ts
@@ -2,7 +2,6 @@ export interface ProductListItem {
     id: number;
     categoryId: number;
     name: string;
-    price: number;
-    description: string;
-    imageUrl: string;
+    quantityPerUnit: string;
+    unitPrice: number;
   }
