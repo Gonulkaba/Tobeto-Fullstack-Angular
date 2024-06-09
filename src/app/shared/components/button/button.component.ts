d66c1684c1bdc7dd01cce78a385319cb2d82ed55
Gönül Kaba <gonul.kaba123@gmail.com>
2024-06-09 13:40:33 +0300

    add logged and logout view in navbar

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,5 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -13,4 +19,5 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 })
 export class ButtonComponent {
   @Input() type: 'button' | 'submit' | 'reset' = 'button';
+  @Output() buttonClick = new EventEmitter<MouseEvent>();
 }
